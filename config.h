068277520039b7a5741a742877a68b916e404e53
genomicsoup <95888891+genomicsoup@users.noreply.github.com>
2024-05-04 20:29:26 -0500

    initial commit

---
+++ config.h
@@ -0,0 +1,22 @@
+/*
+ * file: config.c
+ * desc: Configure keyboard features.
+ */
+#pragma once
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define NO_ACTION_TAPPING
+//#define NO_DEBUG
+//#define NO_PRINT
+
+#define SPLIT_USB_DETECT
+#define MASTER_LEFT
+#define SPLIT_WPM_ENABLE
+
+//#define RGB_MATRIX_SLEEP
+#define SPLIT_TRANSPORT_MIRROR
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 100
+
+#define OLED_FONT_H "./keyboards/sofle_choc/keymaps/genomicsoup/glcdfont.c"
+
